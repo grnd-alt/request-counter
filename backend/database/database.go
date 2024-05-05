3a0d3c75ce0ddf6d73da39e3083d8538e92a3d15
grnd-alt <salimbelakkaf@outlook.de>
2024-05-05 23:04:30 +0200

    retry db connection if ping fails

--- backend/database/database.go
+++ backend/database/database.go
@@ -5,16 +5,30 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 var db *sql.DB
 
 func Connect() {
 	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_URL"), os.Getenv("DB_NAME"))
+	fmt.Println("CONNECTING NOW")
 	var err error
 	db, err = sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("could not open", err)
+	}
+	pingerr := db.Ping()
+	if pingerr != nil {
+		fmt.Println("db not responding, retrying in 5 seconds")
+		time.Sleep(5 * time.Second)
+		for pingerr != nil {
+			db, err = sql.Open("postgres", connStr)
+			pingerr = db.Ping()
+			if err != nil {
+				log.Fatal("could not open", err)
+			}
+		}
 	}
 	ApplyMigrations()
 }
